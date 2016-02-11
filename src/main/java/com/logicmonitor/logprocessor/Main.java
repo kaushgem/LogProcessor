c17c553709a84cb58ecc72ce941385fab600ec62
kaushgem <kaushcs@gmail.com>
2016-02-10 21:20:18 -0800

    Complete Java Doc

--- src/main/java/com/logicmonitor/logprocessor/Main.java
+++ src/main/java/com/logicmonitor/logprocessor/Main.java
@@ -2,6 +2,13 @@ package com.logicmonitor.logprocessor;
 
 import com.logicmonitor.logprocessor.service.LogProcessorService;
 
+/**
+ * Main method
+ * args[0] - Path of log file directory 
+ * args[1] - number of threads
+ * @author kaush
+ *
+ */
 public class Main {
 	public static void main(String[] args) {
 		LogProcessorService.LOGPROCESSOR.LogProcessor(args[0], Integer.parseInt(args[1]));
